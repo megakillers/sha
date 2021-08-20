c15dad4358a623b55c54f070ea246899ca1b8940
megakillers <87588668+megakillers@users.noreply.github.com>
2021-08-20 12:52:47 -0500

    Add files via upload

---
+++ src/desmenu.js
@@ -0,0 +1,32 @@
+const desmenu = (prefix, pushname) => {
+    return `
+*Comandos De Descargas ✅*
+ 
+══════════════
+
+*AUDIO*
+
+${prefix}play2
+Una alternativa por si *play llego a su limite de canciones
+
+${prefix}play
+Coloca el nombre de la cancion, o el link del video YT
+
+
+${prefix}play Industry Baby - Lil Nas X
+
+══════════════
+
+*COMUNICADO*
+
+*ytmp4 y *ytmp3 estan en mantenimiento, muy pronto sacare una actualizacion de esos comandos mas renovado :)
+
+══════════════
+    
+_*play y *play2 tiene un limite de 2000 canciones en el server, si llega a su limite se restablecera dentro de 24 horas_
+     
+ву ѕнαη∂υу`
+
+}
+
+exports.desmenu = desmenu
---
+++ src/version.js
@@ -0,0 +1,27 @@
+const version = (prefix, pushname) => {
+    return `
+*ѕнαηвσт ву ѕнαη∂υу*
+
+*Actualizado:* 18 de Agosto del 2021
+*Versión actual:* 3.7
+*Ofrecida por:* ѕнαη∂υу™
+
+
+*INFORME*
+
+Si no funciona el comando *play o *play2 checa el blog del bot y mira la version que poses 
+Ya que ire cambiando mi apikey de mi bot para que siga en funcionamiento
+Cambiare la apikey pasando un mes o si veo muchos atercados o robos
+Si vez a alguien que robo mi bot comunicate conmigo con el comando *creador
+
+
+*COMO ACTUALIZAR*
+
+https://cutt.ly/jmSF2RN
+
+ву ѕнαη∂υу
+`
+
+}
+
+exports.version = version
